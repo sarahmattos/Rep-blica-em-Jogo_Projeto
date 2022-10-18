fd6222884741fe0c7b02d7eb6bbbfb3397957545
Sarah Mattos <sahmattos13@gmail.com>
2022-10-18 16:14:09 -0300

    multiplayer samples utilities

--- Republica_Em_Jogo/Assets/netCode/script/PlayerNetwork.cs
+++ Republica_Em_Jogo/Assets/netCode/script/PlayerNetwork.cs
@@ -5,10 +5,46 @@ using Unity.Netcode;
 
 public class PlayerNetwork : NetworkBehaviour
 {
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(
+        new MyCustomData
+        {
+            _int =56,
+            _bool=false,
+        }, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    
+
+    public struct MyCustomData :INetworkSerializable
+    {
+        public int _int;
+        public bool _bool;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T: IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+        }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) =>
+        {
+            Debug.Log(OwnerClientId + "; randomNumber: " + newValue._int+"; "+newValue._bool);
+        };
+    }
+
     private void Update()
     {
+
         if (!IsOwner) return;
 
+        if (Input.GetKeyDown(KeyCode.T)){
+            randomNumber.Value = new MyCustomData{
+                _int = 10,
+                 _bool = true,
+            };
+         }
         Vector3 moveDir = new Vector3(0, 0, 0);
 
         if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
