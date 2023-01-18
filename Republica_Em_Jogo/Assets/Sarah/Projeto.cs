4df922348650ff0b5a8324ff8e117d4351700520
Sarah Mattos <sahmattos13@gmail.com>
2023-01-18 15:16:02 -0300

    comentando e limpando "projeto"

--- Republica_Em_Jogo/Assets/Sarah/Projeto.cs
+++ Republica_Em_Jogo/Assets/Sarah/Projeto.cs
@@ -46,10 +46,10 @@ using Unity.Collections;
         public void Awake(){
             setUpZona = GameObject.FindObjectOfType<SetUpZona>();
             zt = GameObject.FindObjectOfType<ZonaTerritorial>();
-            
             hs = FindObjectOfType<HudStatsJogador>();
         }
 
+        //reseta valores para nova busca
         [ServerRpc(RequireOwnership = false)]
         public void DefaultValuesServerRpc()
         {
@@ -60,7 +60,8 @@ using Unity.Collections;
             zonaNetworkName.Value="";
         }
 
-    [ServerRpc(RequireOwnership = false)]
+        //pede pro host avaliar a votação
+        [ServerRpc(RequireOwnership = false)]
         public void UpdateVotacaoServerRpc(int valor)
         {
             if(valor==0){
@@ -70,16 +71,16 @@ using Unity.Collections;
                 votacaoRespostaContra.Value++;
             }
             
-            
         }
+
+        //host atualiza zona escolhida
         [ServerRpc(RequireOwnership = false)]
         public void UpdateZonaServerRpc(string clientDados)
         {
             zonaNetworkName.Value =clientDados;
-            //Debug.Log(zonaNetworkName.Value);
-            
         }
-    //
+    
+        //hosta atualiza a carta de projeto para os clientes
         [ServerRpc(RequireOwnership = false)]
         public void UpdateClientPositionServerRpc(string clientDados, int clientId, int num)
         {
@@ -87,13 +88,10 @@ using Unity.Collections;
         recompensaNetworkNum.Value=num;
         idPlayer.Value= clientId;
         numPlayers.Value=NetworkManager.Singleton.ConnectedClientsIds.Count;
-        
         }
         
-
+        //sortea os valores do projeto
         public void sortearProjeto(){
-            //textoprojeto
-            //se for seu turno
             defaultValues();
             proposta = projetoManager.proposta[Random.Range(0, projetoManager.proposta.Length)];
             numRecompensa = projetoManager.numRecompensa[Random.Range(0, projetoManager.numRecompensa.Length)];
@@ -102,6 +100,7 @@ using Unity.Collections;
             atualizarProjeto(textoTotal);
             
         }
+        //atualiza a carta de projeto ou pede pro host fazer isso
         public void atualizarProjeto(string textoTotal2){
             if(NetworkManager.Singleton.IsClient){  
                 int id =(int)NetworkManager.Singleton.LocalClientId;
@@ -117,163 +116,185 @@ using Unity.Collections;
                 
             }
         }
+        //verifica valores das variaves network se mudaram
         private void OnEnable()
         {
+            //jogadores conectados
             numPlayers.OnValueChanged += (int  previousValue, int  newValue) =>
             {
-            
                 numPlayer=newValue;
-                
-                
             };
 
+            //id jogador
             idPlayer.OnValueChanged += (int  previousValue, int  newValue) =>
             {
+                //interface geral
                 clienteLocal=newValue;
-                //hs.updateRecursoCartaUI(numRecompensa, clienteLocal);
                 projetoUI.SetActive(true);
                 verProjetoBtn.SetActive(false);
                 fecharBtn.SetActive(false);
+
+                //interface para quem está escolhendo zona
                 bntsUi.SetActive(true);
                 text_avisoProjeto.text="Escolha uma zona:";
+
+                //interface para quem está esperando zona ser escolhida
                 if(newValue!=(int)NetworkManager.Singleton.LocalClientId){
                         bntsUi.SetActive(false);
                         text_avisoProjeto.text="Aguardando zona ser escolhida";
                 }
-
-                
-                
             };
+            //valor da recompensa
             recompensaNetworkNum.OnValueChanged += (int  previousValue, int  newValue) =>
             {
                 if(newValue!=-1){
                     numRecompensa= newValue;
                     numRecompensaDefault=numRecompensa;
                 }
-            
             };
+            //texto do projeto
             projetoNetworkTexto.OnValueChanged += (FixedString4096Bytes  previousValue, FixedString4096Bytes  newValue) =>
             {
                 if(newValue!=""){
                     text_projetoCarta.text =  newValue.ToString();
                 }
-                
             };
+            //zona escolhida
             zonaNetworkName.OnValueChanged += (FixedString4096Bytes  previousValue, FixedString4096Bytes  newValue) =>
             {
                 zonaNameLocal=newValue.ToString();
                 if(newValue!=""){
-                bntsUi.SetActive(false);
-                if(clienteLocal==(int)NetworkManager.Singleton.LocalClientId){
-                text_avisoProjeto.text = "\n"+"\n"+"\n"+"Zona escolhida: "+newValue.ToString()+"\n"+"Aguardando votação... ";
-                }else{
+
+                    //desativa zonas para ser escolhidas
+                    bntsUi.SetActive(false);
+                    
+                    //interface para quem escolheu zona e está esperando votação
+                    if(clienteLocal==(int)NetworkManager.Singleton.LocalClientId){
+                    text_avisoProjeto.text = "\n"+"\n"+"\n"+"Zona escolhida: "+newValue.ToString()+"\n"+"Aguardando votação... ";
+
+                    //interface para quem está votando
+                    }else{
                     text_avisoProjeto.text = "\n"+"\n"+"Zona escolhida: "+newValue.ToString()+"\n"+"Vote: ";
                     btns2.SetActive(true);
-                }
+                    }
                 }
                 
             };
             
+            //votacao a favor
             votacaoRespostaFavor.OnValueChanged += (int  previousValue, int  newValue) =>
             {
                 sim=newValue;
                 
             };
+
+            //votacao contra
             votacaoRespostaContra.OnValueChanged += (int  previousValue, int  newValue) =>
             {
                 nao=newValue;
                 
             };
-            
-
         }
         public void Update(){
+
+            //quantidade de jogadores conectados
             if(numPlayer>1){
-            if(sim+nao>=numPlayer-1 ){
-                //Debug.Log("numPlayer"+numPlayer);
-                //Debug.Log("nao"+nao);
-                btns2.SetActive(false);
-                fecharBtn.SetActive(true);
-                if(sim>nao){
-                    text_avisoProjeto.text="\n"+"\n"+"\n"+"PROJETO APROVADO"+"\n"+"Recompensa: "+numRecompensaDefault+ " carta(s) e "+numRecompensaDefault+" eleitor(es)";
-                    eleitoresZonaFinal();
-                }
-                if(nao>=sim){
-                text_avisoProjeto.text="\n"+"\n"+"\n"+"PROJETO NÃO APROVADO";
+
+            //se todos votaram
+                if(sim+nao>=numPlayer-1 ){
+
+                    //desativa botão de votação
+                    btns2.SetActive(false);
+                    //ativa botao de fechar interface
+                    fecharBtn.SetActive(true);
+
+                    //se teve mais sim, foi aprovado
+                    if(sim>nao){
+                        text_avisoProjeto.text="\n"+"\n"+"\n"+"PROJETO APROVADO"+"\n"+"Recompensa: "+numRecompensaDefault+ " carta(s) e "+numRecompensaDefault+" eleitor(es)";
+                        //parte da recompensa
+                        eleitoresZonaFinal();
+                    }
+
+                    //se teve mais não ou empate, foi reprovado
+                    if(nao>=sim){
+                    text_avisoProjeto.text="\n"+"\n"+"\n"+"PROJETO NÃO APROVADO";
+                    }
                 }
             }
-        }
             
         }
         
+        //chamado apos projeto ser aprovado
         public void eleitoresZonaFinal(){
+
+            //adiciona eleitores nos bairros da zona
              if (NetworkManager.Singleton.IsServer){
-            setUpZona.eleitoresZona(numRecompensa, zonaNameLocal);
+                setUpZona.eleitoresZona(numRecompensa, zonaNameLocal);
              }
+            
+            //verifica se player tem bairro na zona escolhida
             setUpZona.playerZona(NetworkManager.Singleton.LocalClientId, zonaNameLocal);
+
+            //dá carta de recurso para jogadores que possuem bairros na zona
             if(playerInZona==true){
-                Debug.Log("playerINzona true");
                 hs.updateRecursoCartaUI(numRecompensaDefault);
                 playerInZona=false;
-            }else{
-               // Debug.Log("playerINzona false");
             }
             
+            //reseta algumas variáveis
             zonaNameLocal="";
             clienteLocal=-1;
             numRecompensa=-1;
             }
             
+         //ao apertar botao de fechar interface   
         public void fechar(){
+            //desatuva interface
             fecharBtn.SetActive(false);
             projetoUI.SetActive(false);
-            
-            
         }
+
+        //reseta variaveis oou pede pro hosta fazer isso
         public void defaultValues(){
             sim=0;
             nao=0;
             mostrouResultado=false;
             if (NetworkManager.Singleton.IsServer){
-            recompensaNetworkNum.Value=-1;
-            votacaoRespostaFavor.Value=0;
-            votacaoRespostaContra.Value=0;
-            projetoNetworkTexto.Value="";
-            zonaNetworkName.Value="";
-
+                recompensaNetworkNum.Value=-1;
+                votacaoRespostaFavor.Value=0;
+                votacaoRespostaContra.Value=0;
+                projetoNetworkTexto.Value="";
+                zonaNetworkName.Value="";
             }
             if(NetworkManager.Singleton.IsClient){ 
                 DefaultValuesServerRpc();
             }
-            
         }
 
+        //funcao disparada ao apertar no nome de uma zona
         public void escolherZona(string zonaName){
             if (NetworkManager.Singleton.IsServer){
-            
                 zonaNetworkName.Value =zonaName;
-                //Debug.Log(zonaNetworkName.Value);
             }
             if(NetworkManager.Singleton.IsClient){ 
                 UpdateZonaServerRpc(zonaName);
             }
             
         }
+
+        //funcao disparada ao apertar no botao de a favor ou contra a votação
         public void votacao(int resposta){
 
-                if(NetworkManager.Singleton.IsClient){
-                    UpdateVotacaoServerRpc(resposta);
-                }
-                
-                if(resposta==0)mostrarResposta="a favor";
-                if(resposta==1)mostrarResposta="contra";
-                btns2.SetActive(false);
-                if(numPlayer>2){
-                    text_avisoProjeto.text = "\n"+"\n"+"\n"+"Seu partido votou "+mostrarResposta+"\n"+"Aguardando outros partidos...";
-                }
-                
-                
+            if(NetworkManager.Singleton.IsClient){
+                UpdateVotacaoServerRpc(resposta);
+            }
+            //texto interface recebe valores e botoees somem
+            if(resposta==0)mostrarResposta="a favor";
+            if(resposta==1)mostrarResposta="contra";
+            btns2.SetActive(false);
+            if(numPlayer>2){
+                text_avisoProjeto.text = "\n"+"\n"+"\n"+"Seu partido votou "+mostrarResposta+"\n"+"Aguardando outros partidos...";
+            }
         }
-    
     }
-//}
+
