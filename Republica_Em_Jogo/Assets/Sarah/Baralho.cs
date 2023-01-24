5690a4d4dfb81119e211c9f02fbbff861dc64706
Sarah Mattos <sahmattos13@gmail.com>
2023-01-24 09:35:31 -0300

    baralho sorteia projeto + MS + corrupção

--- Republica_Em_Jogo/Assets/Sarah/Baralho.cs
+++ Republica_Em_Jogo/Assets/Sarah/Baralho.cs
@@ -7,16 +7,29 @@ public class Baralho : MonoBehaviour
 {
     private Renderer renderer;
     private Projeto projeto;
+    private Corrupcao corrupcao;
+    private MovimentosSociais movimentosSociais;
 
     void Start(){
          projeto = FindObjectOfType<Projeto>();
          renderer = GetComponent<Renderer>();
+         corrupcao = FindObjectOfType<Corrupcao>();
+         movimentosSociais = FindObjectOfType<MovimentosSociais>();
     }
     
     private void OnMouseDown()
     {
+        if (!enabled) return;
         Debug.Log("clicado");
-        projeto?.sortearProjeto();
+        int rnd = Random.Range(0,100);
+        Debug.Log(rnd);
+        if(rnd>=0 && rnd<25)corrupcao?.sortearCorrupcao();
+        if(rnd>=25 && rnd<50)movimentosSociais?.sortearMS();
+        if(rnd>=50)projeto?.sortearProjeto();
+        this.enabled = false;
+        
+        
+        
         //cartaProjetoTrue();
     }
     
