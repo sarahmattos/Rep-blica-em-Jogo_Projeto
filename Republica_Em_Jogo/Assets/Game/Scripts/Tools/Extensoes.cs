5eb9b3c1a43fcc38839e0d7d442f12a9ca66c193
Alexandregaudencio <alexandre.gtpjr@gmail.com>
2023-02-01 11:22:33 -0300

    Remove: logs excessivos; pacotes em desuso

--- Republica_Em_Jogo/Assets/Game/Scripts/Tools/Extensoes.cs
+++ Republica_Em_Jogo/Assets/Game/Scripts/Tools/Extensoes.cs
@@ -42,8 +42,6 @@ namespace Game.Tools
 
         public static void AddAll<T>(this IList<T> list, IList<T> newValues)
         {
-            Debug.Log("List: "+list.Count);
-            Debug.Log("new list: " + newValues.Count);
             foreach (T values in newValues)
             {
                 list.Add(values);
