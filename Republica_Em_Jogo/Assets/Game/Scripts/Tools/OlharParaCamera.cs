10d0478f9f42cf60fbde92fe167dceed777ee696
Alexandregaudencio <alexandre.gtpjr@gmail.com>
2023-02-13 10:31:08 -0300

    Canvas lookAt in EditorMode

--- Republica_Em_Jogo/Assets/Game/Scripts/Tools/OlharParaCamera.cs
+++ Republica_Em_Jogo/Assets/Game/Scripts/Tools/OlharParaCamera.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[ExecuteInEditMode]
 public class OlharParaCamera : MonoBehaviour
 {
     [SerializeField] private bool active;
