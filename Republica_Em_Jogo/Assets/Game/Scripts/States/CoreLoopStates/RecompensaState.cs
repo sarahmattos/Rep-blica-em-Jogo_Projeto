633d90b4ef28ed6b11aad1ac2eedcb4938ebdcdb
Alexandregaudencio <alexandre.gtpjr@gmail.com>
2023-03-01 16:20:50 -0300

    Fix: Recompensa correta no recompensa State

--- Republica_Em_Jogo/Assets/Game/Scripts/States/CoreLoopStates/RecompensaState.cs
+++ Republica_Em_Jogo/Assets/Game/Scripts/States/CoreLoopStates/RecompensaState.cs
@@ -4,20 +4,24 @@ using Game.Player;
 using Game.Territorio;
 using UnityEngine;
 using System.Linq;
+using Game.UI;
 
 namespace Game
 {
     public class RecompensaState : State
     {
+        [SerializeField] private HudStatsJogador hudStatsJogador;
 
         [SerializeField] private AvancoState avancoState;
+        [SerializeField] private int qntdRecurso = 1;
         private AvancoData avancoData => avancoState.AvancoData;
-        private const int recompensaEleitores = 2;
-        private int qntdEleitorAplicado = 0;
+        // private const int recompensaEleitores = 2;
+        // private int qntdEleitorAplicado = 0;
         public bool TemRecompensa =>  (avancoData.BairrosAdquiridos) > 0 ? true : false;
-        public bool AplicouTodosEleitores => qntdEleitorAplicado == recompensaEleitores;
+        // public bool AplicouTodosEleitores => qntdEleitorAplicado == recompensaEleitores;
         public PlayerStats PlayerStatsAtual =>  PlayerStatsManager.Instance.GetPlayerStatsDoPlayerAtual();
         public List<Bairro> bairrosDoPlayer => PlayerStatsAtual?.BairrosInControl;
+
         
 
         public override void EnterState()
@@ -26,18 +30,22 @@ namespace Game
             if(!TemRecompensa) {
                 Tools.Logger.Instance.LogInfo("Como não avançou em nenhum bairro, não há recompensa nesta rodada.");
                 return;
-            } 
-            qntdEleitorAplicado = 0;
-            HabilitarBairros(true);
-            InscreverBairros();
+            }
+            hudStatsJogador.updateRecursoCartaUI(qntdRecurso);
+            
+
+            
+            // qntdEleitorAplicado = 0;
+            // HabilitarBairros(true);
+            // InscreverBairros();
 
         }
 
         public override void ExitState()
         {
             Tools.Logger.Instance.LogInfo("Exit RECOMPENSA");
-            HabilitarBairros(false);
-            DesincreverBairros();
+            // HabilitarBairros(false);
+            // DesincreverBairros();
         }
 
         private void HabilitarBairros(bool value) {
@@ -46,35 +54,35 @@ namespace Game
             }
         }
 
-        private void InscreverBairros() {
-            foreach (Bairro bairro in bairrosDoPlayer)
-            {
-                bairro.Interagivel.click += OnClick;
-            }
-        }
+        // private void InscreverBairros() {
+        //     foreach (Bairro bairro in bairrosDoPlayer)
+        //     {
+        //         bairro.Interagivel.click += OnClick;
+        //     }
+        // }
 
-        private void DesincreverBairros() {
-            foreach (Bairro bairro in bairrosDoPlayer)
-            {
-                bairro.Interagivel.click -= OnClick;
-            }
-        }
+        // private void DesincreverBairros() {
+        //     foreach (Bairro bairro in bairrosDoPlayer)
+        //     {
+        //         bairro.Interagivel.click -= OnClick;
+        //     }
+        // }
 
-        private void AplicarRecompensa(Bairro bairro) {
-            bairro.SetUpBairro.Eleitores.AcrescentaEleitorServerRpc(1);
-            qntdEleitorAplicado++;     
+        // private void AplicarRecompensa(Bairro bairro) {
+        //     bairro.SetUpBairro.Eleitores.AcrescentaEleitorServerRpc(1);
+        //     qntdEleitorAplicado++;     
 
-        }
+        // }
 
-        private void OnClick(Bairro bairro) {
-            if(AplicouTodosEleitores) {
-                Tools.Logger.Instance.LogInfo("Recompensas aplicadas.");
-                return;
-            }
-            AplicarRecompensa(bairro);
+        // private void OnClick(Bairro bairro) {
+        //     if(AplicouTodosEleitores) {
+        //         Tools.Logger.Instance.LogInfo("Recompensas aplicadas.");
+        //         return;
+        //     }
+        //     AplicarRecompensa(bairro);
 
             
-        }
+        // }
 
 
 
