344acb1e5c865870284fc2473c5c08e812cd13e6
Alexandregaudencio <alexandre.gtpjr@gmail.com>
2023-03-04 09:03:05 -0300

    Feat: Sync Load gameplay scene

---
+++ Republica_Em_Jogo/Assets/Game/Scripts/Gameplay Connection Manager/SyncCarregamentoGameplayScene.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Game
+{
+    public class SyncCarregamentoGameplayScene : MonoBehaviour
+    {
+        [SerializeField] private Canvas CanvasUiCarregamento;
+        [SerializeField] private TMP_Text LoadingMessageText;
+        [SerializeField] private string message;
+
+        public State GameplaySceneLoad => GameStateHandler.Instance.StatePairValue[GameState.GAMEPLAY_SCENE_LOAD];
+
+        private void Start()
+        {
+            CanvasUiCarregamento.enabled = true;
+            LoadingMessageText.SetText(message);
+            GameplaySceneLoad.Saida += OnGameplaySceneLoad;
+        }
+
+        private void OnDestroy()
+        {
+            GameplaySceneLoad.Saida -= OnGameplaySceneLoad;
+
+        }
+
+
+        private void OnGameplaySceneLoad()
+        {
+            CanvasUiCarregamento.enabled = false;
+            LoadingMessageText.SetText("");
+        }
+
+
+
+
+    }
+}
