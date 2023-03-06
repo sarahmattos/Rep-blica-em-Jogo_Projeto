7e1dd760e6d953374504659b2e088e5cac2953a2
Alexandregaudencio <alexandre.gtpjr@gmail.com>
2023-03-06 16:08:17 -0300

    StateMachine. controle isolado em nova classe.

---
+++ Republica_Em_Jogo/Assets/Game/Scripts/States/StateController/StateMachineController.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Game.Tools;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Game
+{
+    [Serializable]
+    public class StateMachineController : NetworkBehaviour
+    {
+        private NetworkVariable<int> indexState = new NetworkVariable<int>(-1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+        private List<State> states;
+        [SerializeField] private State currentState;
+        public event Action<int> estadoMuda;
+
+        public State GetState(int index)
+        {
+            return states[index];
+        }
+        public State GetCurrentState()
+        {
+            return currentState;
+        }
+
+        public void ResetMachineState() {
+            ChangeStateServerRpc(-1);
+        }
+
+
+        public StateMachineController(List<State> statesOrdenados)
+        {
+            states = statesOrdenados;
+        }
+
+        public void Initialize()
+        {
+            Debug.Log("initialize. State machine controller.");
+            indexState.OnValueChanged += OnIndexStateMuda;
+
+        }
+
+        public void Finish()
+        {
+            indexState.OnValueChanged += OnIndexStateMuda;
+            if (!NetworkManager.Singleton.IsHost) return;
+            indexState.Dispose();
+        }
+
+        private void OnIndexStateMuda(int previous, int next)
+        {
+            currentState?.InvokeSaida();
+            currentState = states[next];
+            currentState.InvokeEntrada();
+            estadoMuda?.Invoke(next);
+
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        public void ChangeStateServerRpc(int newIndex)
+        {
+            indexState.Value = newIndex;
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        public void NextStateServerRpc()
+        {
+            indexState.Value = (indexState.Value +1) % states.Count;
+
+        }
+
+
+
+    }
+}
