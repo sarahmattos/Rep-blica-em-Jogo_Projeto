da71ce17b9ebde832b3f9838cc8184dca9b80c6b
Alexandregaudencio <alexandre.gtpjr@gmail.com>
2023-04-13 13:38:43 -0300

    Update: SectRectWidth com anim

--- Republica_Em_Jogo/Assets/Game/Scripts/UI/UIIconeCorJogador.cs
+++ Republica_Em_Jogo/Assets/Game/Scripts/UI/UIIconeCorJogador.cs
@@ -1,23 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Game.Player;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Game.UI
 {
+
     public class UIIconeCorJogador : MonoBehaviour
     {
         [SerializeField] private int playerID;
         [SerializeField][Range(0.2f, 1)] private float MinWidthPercentage = 0.5f;
         [SerializeField][Range(0.2f, 1)] private float MaxWidthPercentage = 1;
-        public RectTransform rectTransform;
-        public RectTransform parentRectTransform;
+        private RectTransform rectTransform;
+        private RectTransform parentRectTransform;
         private Image image;
         private VerticalLayoutGroup verticalLayoutGroup;
         public float MinWidth => parentRectTransform.sizeDelta.x * MinWidthPercentage;
         public float MaxWidth => parentRectTransform.sizeDelta.x * MaxWidthPercentage - verticalLayoutGroup.padding.left;
+
 
         private void GetReferenceComponents()
         {
@@ -87,14 +90,41 @@ namespace Game.UI
 
         }
 
-        private void SetRectWidth(float width)
+        private async void SetRectWidth(float width)
         {
-            Vector2 sizeDelta = rectTransform.sizeDelta;
-            sizeDelta.x = width;
-            rectTransform.sizeDelta = sizeDelta;
+
+            Vector2 targetSizeDelta = rectTransform.sizeDelta;
+            targetSizeDelta.x = width;
+            Vector2 velocityDamp = Vector2.one;
+            float elapsedTime = 0f;
+            float timeInterval = 0.5f;
+
+            while (elapsedTime < timeInterval)
+            {
+                rectTransform.sizeDelta = Vector2.SmoothDamp(
+                    rectTransform.sizeDelta,
+                    targetSizeDelta,
+                    ref velocityDamp,
+                    timeInterval*timeInterval*100*Time.deltaTime
+                );
+
+                elapsedTime += Time.deltaTime;
+                await Task.Delay((int)(Time.deltaTime * 1000));
+            }
+
         }
 
-
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.N))
+            {
+                SetRectWidth(MinWidth);
+            }
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                SetRectWidth(MaxWidth);
+            }
+        }
 
     }
 }
