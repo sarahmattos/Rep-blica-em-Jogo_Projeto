3d9d8e84c6f2a313933cfe2f7da8664060334b07
Sarah Mattos <sahmattos13@gmail.com>
2023-04-26 16:58:48 -0300

    dados na interface funcionando

--- Republica_Em_Jogo/Assets/Game/Scripts/UI/UIAvancoState.cs
+++ Republica_Em_Jogo/Assets/Game/Scripts/UI/UIAvancoState.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 namespace Game.UI
@@ -10,6 +11,9 @@ namespace Game.UI
         [SerializeField] private TMP_Text text_dadosVizinhos;
         [SerializeField] private TMP_Text text_dadosPlayerAtual;
         [SerializeField] private AvancoState avancoState;
+        [SerializeField] private Image[] dadosImageSeus;
+        [SerializeField] private Image[] dadosImageVizinhos;
+        [SerializeField] private Sprite[] dadosSprite;
         DadosUiGeral dadosUiGeral;
         public AvancoData AvancoData => avancoState.AvancoData;
 
@@ -25,12 +29,16 @@ namespace Game.UI
         }
 
         private void UpdateTextDados() {
-            string dadosPlayerAtual = "Ataque: \n";
-            string dadosVizinhos = "Defesa: \n";
+            
+            //string dadosPlayerAtual = "Ataque: \n";
+            //string dadosVizinhos = "Defesa: \n";
+             string dadosPlayerAtual="";
+             string dadosVizinhos="";
+            
             foreach(int value in AvancoData.DadosPlayerAtual)
-                dadosPlayerAtual = string.Concat(dadosPlayerAtual, value, "\n");
+                dadosPlayerAtual = string.Concat(dadosPlayerAtual, value ,";");
             foreach(int value in AvancoData.DadosVizinhos)
-                dadosVizinhos = string.Concat(dadosVizinhos, value, "\n");
+                dadosVizinhos = string.Concat(dadosVizinhos, value ,";");
             
             //text_dadosPlayerAtual.SetText(dadosPlayerAtual);
             //text_dadosVizinhos.SetText(dadosVizinhos);  
@@ -39,8 +47,34 @@ namespace Game.UI
 
         }
         public void UpdateTextDados2(string _p,string _v){
-            text_dadosPlayerAtual.SetText(_p);
-            text_dadosVizinhos.SetText(_v);   
+
+            foreach(Image img in dadosImageSeus){
+                img.gameObject.SetActive(false);
+            }
+            foreach(Image img in dadosImageVizinhos){
+                img.gameObject.SetActive(false);
+            }
+
+            string[] valoresSeparados = _p.Split(';');
+            string[] valoresSeparados2 = _v.Split(';');
+            int teste;
+
+            if (int.TryParse(valoresSeparados[0], out teste))
+            {
+                for(int i=0; i<valoresSeparados.Length-1;i++){
+                    dadosImageSeus[i].sprite = dadosSprite[int.Parse(valoresSeparados[i])];
+                    dadosImageSeus[i].gameObject.SetActive(true);
+                }
+            
+                for(int i=0; i<valoresSeparados2.Length-1;i++){
+                    dadosImageVizinhos[i].sprite =  dadosSprite[int.Parse(valoresSeparados2[i])];
+                    dadosImageVizinhos[i].gameObject.SetActive(true);
+                }
+            } 
+            
+            
+            text_dadosPlayerAtual.SetText("Ataque:");
+            text_dadosVizinhos.SetText("Defesa:");   
             
         }
 
