e98f8597c0006685283e0c3c2fc4c265f6da7a69
Sarah Mattos <sahmattos13@gmail.com>
2023-05-18 16:32:10 -0300

    fix bugs 18/05 part final
    TAB arrumado os nomes

--- Republica_Em_Jogo/Assets/Sarah/infoGeralPlayers/infoGeral.cs
+++ Republica_Em_Jogo/Assets/Sarah/infoGeralPlayers/infoGeral.cs
@@ -18,6 +18,7 @@ namespace Game
         private NetworkVariable<int> pedeInfo = new NetworkVariable<int>();
         string info0, info1, info2, info3;
         [SerializeField] private TMP_Text teste0, teste1, teste2, teste3;
+        [SerializeField] private TMP_Text nome0, nome1, nome2, nome3;
         [SerializeField] private GameObject infoGo;
         PlayerStats ps;
         HudStatsJogador hs;
@@ -25,6 +26,10 @@ namespace Game
         void Start()
         {
             hs = FindObjectOfType<HudStatsJogador>();
+            info0 = " ; ";
+            info1 = " ; ";
+            info2 = " ; ";
+            info3 = " ; ";
         }
         [ServerRpc(RequireOwnership = false)]
         public void pedeInfoServerRpc(int valor)
@@ -103,8 +108,8 @@ namespace Game
             //  textoTotal = ps.Nome+ ": "+ps.EleitoresTotais+" eleitores, "+ps.BairrosInControl.Count.ToString()+
             //" bairros, "+ps.numSaude+" cartas de saúde, "+ps.numEducacao+" cartas de educação, "+ps.numCadeiras+
             // " cadeiras";
-            textoTotal = GameDataconfig.Instance.TagPlayerColorizada(ps) + ":       " + ps.numSaude + "          " + ps.numEducacao + "          " + ps.numCadeiras + "        " +
-            ps.EleitoresTotais + "        " + ps.BairrosInControl.Count.ToString();
+            textoTotal = GameDataconfig.Instance.TagPlayerColorizada(ps) + ":; " + ps.numSaude + "            " + ps.numEducacao + "            " + ps.numCadeiras + "          " +
+            ps.EleitoresTotais + "          " + ps.BairrosInControl.Count.ToString();
             if (ps.playerID == 0) AtualizaInfo0GeralServerRpc(textoTotal);
             if (ps.playerID == 1) AtualizaInfo1GeralServerRpc(textoTotal);
             if (ps.playerID == 2) AtualizaInfo2GeralServerRpc(textoTotal);
@@ -114,10 +119,31 @@ namespace Game
         }
         public void updateInfoUi()
         {
-            teste0.text = info0;
-            teste1.text = info1;
-            teste2.text = info2;
-            teste3.text = info3;
+
+            string[] separatedStrings0 = info0.Split(';');
+            if(separatedStrings0.Length>0){
+                nome0.text = separatedStrings0[0];
+                teste0.text = separatedStrings0[1];
+            }
+            
+            
+            string[] separatedStrings1 = info1.Split(';');
+            if(separatedStrings1.Length>0){
+                nome1.text = separatedStrings1[0];
+                teste1.text = separatedStrings1[1];
+            }
+
+            string[] separatedStrings2 = info2.Split(';');
+             if(separatedStrings2.Length>0){
+                nome2.text = separatedStrings2[0];
+                teste2.text = separatedStrings2[1];
+             }
+
+            string[] separatedStrings3 = info3.Split(';');
+            if(separatedStrings3.Length>0){
+                nome3.text = separatedStrings3[0];
+                teste3.text = separatedStrings3[1];
+            }
         }
         public void updateUi(bool valor)
         {
